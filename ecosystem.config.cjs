bb20ec65b55383484387c524dcd9f961f3d6c07b
devkavach22 <developer@kavachglobal.com>
2026-03-09 12:57:27 +0530

    new changes for the deployemnet done

---
+++ ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "sandbox-backend",
+      script: "./server.js",
+
+      instances: "max",
+      exec_mode: "cluster",
+
+      watch: false,
+      autorestart: true,
+      max_memory_restart: "500M",
+
+      env_file: "/etc/node-env/SandBox/.env",
+
+      env: {
+        NODE_ENV: "development"
+      },
+
+      env_production: {
+        NODE_ENV: "production"
+      },
+
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss"
+    }
+  ]
+};
